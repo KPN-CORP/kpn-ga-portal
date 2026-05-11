90ee62e442c98e68319336ee95e311e68b4a96bb
lubun09 <detlin09@gmail.com>
2026-05-11 14:09:05 +0700

    11 May 2026

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -378,7 +378,7 @@
         ],
         'apart' => [
             'field' => 'apart_dash',
-            'title' => 'Apartment Services',
+            'title' => 'E-Services Apartment',
             'url' => 'apartemen',
             'color' => '#d35400',
             'icon' => 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4'
@@ -470,7 +470,7 @@
         ],
         'apart_admin' => [
             'field' => 'apart_admin_dash',
-            'title' => 'GA Apartment Services',
+            'title' => 'GA E-Services Apartment',
             'url' => '/apartemen/admin/dashboard',
             'color' => '#d35400',
             'icon' => 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4'
